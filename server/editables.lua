37c6de66d09c1115b6978f6771966036c3fb3d2f
MilyonJames <clement.dolo@gmail.com>
2023-10-26 19:21:36 +0200

    Now using getSharedObject() export as default

--- server/editables.lua
+++ server/editables.lua
@@ -1,5 +1,6 @@
-ESX = nil
-TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+ESX = exports["es_extended"]:getSharedObject()
+-- ESX = nil
+-- TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
 function GetPoliceCount()
     local players = ESX.GetPlayers()
