8b5e92ab3449b38fc316d71c92ecfce4ff12a8cf
MilyonJames <clement.dolo@gmail.com>
2023-12-30 09:10:59 +0100

    QB-Core Support

---
+++ server/esx_framework.lua
@@ -0,0 +1,34 @@
+if GetResourceState("es_extended") == "started" then
+    print("^5Starting with ESX^0")
+    ESX = exports["es_extended"]:getSharedObject()
+    -- ESX = nil
+    -- TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+    function GetPoliceCount()
+        local players = ESX.GetPlayers()
+        local count = 0
+    
+        for i = 1, #players do
+            local player = ESX.GetPlayerFromId(players[i])
+            if player.job.name == 'lspd' then
+                count = count + 1
+            end
+        end
+    
+        return count
+    end
+    
+    function DiscordLog(player_src, event)
+        local xPlayer = ESX.GetPlayerFromId(player_src)
+        TriggerEvent("discord_manager:Log", "logs-others", "Car Heist:"..event.name, "Player: "..xPlayer.getName().." ("..xPlayer.getIdentifier()..")", "red", true)
+    end
+    
+    function GivePlayerRewards(player_src, earnings)
+        local xPlayer = ESX.GetPlayerFromId(player_src)
+        xPlayer.addAccountMoney('black_money', earnings)
+    end
+    
+    function DoesPlayerHaveItem(player_src, item)
+        return ESX.GetPlayerFromId(player_src).getInventoryItem(item).count > 0
+    end
+end
---
+++ server/qb_framework.lua
@@ -0,0 +1,27 @@
+if GetResourceState("qb-core") == "started" then
+    print("^5Starting with QB-Core^0")
+    QBCore = exports['qb-core']:GetCoreObject()
+
+    function GetPoliceCount()
+        local count = 0
+        for ServerId, Player in ipairs(QBCore.Functions.GetQBPlayers()) do
+            if Player.PlayerData.job.name == "police" then
+                count = count + 1
+            end
+        end
+        return count
+    end
+    
+    function DiscordLog(player_src, event)
+        -- Setup your logs
+    end
+    
+    function GivePlayerRewards(player_src, earnings)
+        QBCore.Functions.GetPlayer(player_src).Functions.AddMoney("cash", earnings)
+    end
+    
+    function DoesPlayerHaveItem(source, item)
+        local player_item = QBCore.Functions.GetPlayer(source).Functions.GetItemByName(item)
+        return player_item and player_item.amount >= 0 or false
+    end
+end
